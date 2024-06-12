3b54271fe290e50832e854d2a45ab90a2aa7b43a
Tamada-Arino <arino.tamada@luxiar.com>
2024-06-12 09:15:39 +0900

    不要な記述を削除しました。 不要な三項演算子を削除しました。

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,8 +1,7 @@
-str = ""
-
 (1..20).each do |n|
-  str += "Fizz" if n % 3 == 0
-  str += "Buzz" if n % 5 == 0
-  str == "" ? (puts n) : (puts str)
-  str = ""
+  print_text = ""
+  print_text += "Fizz" if n % 3 == 0
+  print_text += "Buzz" if n % 5 == 0
+  print_text = n if print_text == ""
+  puts print_text
 end
