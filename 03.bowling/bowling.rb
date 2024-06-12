848813cc2ce9b58086ee9b74bf41727250764207
Tamada-Arino <arino.tamada@luxiar.com>
2024-06-12 09:50:13 +0900

    ボウリングのスコア計算プログラムの作成

---
+++ 03.bowling/bowling.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+scores = ARGV[0].chomp.split(',')
+pitches = scores.length - 1
+
+scores.map! do |score|
+  score == 'X' ? 10 : score.to_i
+end
+
+i = 0
+flame = 1
+result = 0
+
+loop do
+  if flame == 10
+    while i <= pitches
+      result += scores[i]
+      i += 1
+    end
+    break
+  elsif scores[i] == 10
+    result += 10 + scores[i + 1] + scores[i + 2]
+    i += 1
+    flame += 1
+  elsif scores[i] + scores[i + 1] == 10
+    result += 10 + scores[i + 2]
+    i += 2
+    flame += 1
+  else
+    result += scores[i] + scores[i + 1]
+    i += 2
+    flame += 1
+  end
+end
+
+puts result
