3aa30d9097006c3ede6e8b2fb7595d055e2f996c
Tamada-Arino <arino.tamada@luxiar.com>
2024-06-24 15:24:05 +0900

    game.rbにスコア計算追加

--- 06.bowling_object/game.rb
+++ 06.bowling_object/game.rb
@@ -1,6 +1,9 @@
+# frozen_string_literal: true
+
+require_relative 'frame'
+
 class Game
   def initialize(scores)
-    scores_length = scores.length - 1
     i = 0
     @frames = []
     (1..10).each do |frame|
@@ -14,5 +17,36 @@ class Game
         i += 2
       end
     end
+  end
+
+  def result_score
+    result = 0
+    @frames.each.with_index do |frame, i|
+      result += frame.frame_score
+      if i < 9
+        if frame.strike?
+          result += strike_score_bonus(i)
+        elsif frame.spare?
+          result += spare_score_bonus(i)
+        end
+      end
+    end
+    result
+  end
+
+  private
+
+  def strike_score_bonus(i)
+    if @frames[i + 1].strike? && i == 8
+      10 + @frames[i + 1].second_shot.score
+    elsif @frames[i + 1].strike?
+      10 + @frames[i + 2].first_shot.score
+    else
+      @frames[i + 1].first_shot.score + @frames[i + 1].second_shot.score
+    end
+  end
+
+  def spare_score_bonus(i)
+    @frames[i + 1].first_shot.score
   end
 end
