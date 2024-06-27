0afd367d607d77821ca5b016d2f384b1f8cb9754
Tamada-Arino <arino.tamada@luxiar.com>
2024-06-27 15:58:46 +0900

    変数名を変更

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -5,13 +5,13 @@ files = Dir.glob('*').sort
 
 cols = 3
 max_row = (files.length / cols.to_f).ceil
-max_length = files.map(&:length).max
+max_char_length = files.map(&:length).max
 print_files = Array.new(max_row) { Array.new(cols) }
 col = 0
 
 files.each.with_index do |file, i|
   row = i % max_row
-  print_files[row][col] = file.ljust(max_length)
+  print_files[row][col] = file.ljust(max_char_length)
   col += 1 if row == max_row - 1
 end
 
