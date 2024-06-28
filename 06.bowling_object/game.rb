749c5fc2809fd5331e519c45242d268406a84685
Tamada-Arino <arino.tamada@luxiar.com>
2024-06-28 15:18:02 +0900

    変数名を変更

--- 06.bowling_object/game.rb
+++ 06.bowling_object/game.rb
@@ -6,14 +6,14 @@ class Game
   def initialize(scores)
     score_index = 0
     @frames = []
-    (1..10).each do |frame|
-      if frame == 10
-        @frames << Frame.new(frame, scores[score_index], scores[score_index + 1], scores[score_index + 2])
+    (1..10).each do |frame_number|
+      if frame_number == 10
+        @frames << Frame.new(frame_number, scores[score_index], scores[score_index + 1], scores[score_index + 2])
       elsif scores[score_index] == 'X'
-        @frames << Frame.new(frame, 'X', 0)
+        @frames << Frame.new(frame_number, 'X', 0)
         score_index += 1
       else
-        @frames << Frame.new(frame, scores[score_index], scores[score_index + 1])
+        @frames << Frame.new(frame_number, scores[score_index], scores[score_index + 1])
         score_index += 2
       end
     end
